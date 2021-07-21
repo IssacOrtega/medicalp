a848d3ca1f5ef50f83dcc6b8df797c65c0d62dbd
Issac Ortega <gissac.ortega@gmail.com>
2021-07-21 04:26:37 -0500

    Se agregaron los archivos y los condicionales requeridos en dashboard/index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,6 +1,22 @@
 <?php
 
+// Declarando la funcion de sesion iniciada
 session_start();
 
-echo 'Bienvenido ' . $_SESSION['user'] . ' eres ' . $_SESSION['roll'] . $_SESSION['id_user'] .'<br>';
-echo 'Pantalla de dashboard';
+// Llamando archvio de configuracion
+require '../data/config.php';
+
+// Llamando al archivo de funciones
+require '../data/functions.php';
+
+
+
+
+/* Condicional para verficar si las variable de sesión de usuario esta seteada 
+nos redireccione a la pagina de inicio de la aplicación */
+if (!isset($_SESSION['user'])) {
+    header('Location: ' . RUTA);
+}
+
+// LLamando al archivo de vista de inicio de sesión
+require 'views/index.view.php';
