46dc09512ce81eaa8774dfb7654af057a4a6f018
Issac Ortega <gissac.ortega@gmail.com>
2021-07-21 00:53:36 -0500

    Se agreco el icono de pestaña en index.view.php

--- resource/views/index.view.php
+++ resource/views/index.view.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="icono.ico">
     <title>Iniciar Sesión</title>
     <!-- Bootstrap core CSS -->
     <link href="<?php echo RUTA . 'resource/assets/css/bootstrap.min.css'; ?>" rel="stylesheet">
