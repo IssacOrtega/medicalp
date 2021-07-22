81dbf772b5d2902356a7d512215d162ed09fcf86
Issac Ortega <gissac.ortega@gmail.com>
2021-07-22 00:37:40 -0500

    Se agregaron las funciones para el modal de eliminar

--- resource/assets/js/dashboard.js
+++ resource/assets/js/dashboard.js
@@ -2,8 +2,8 @@ $('#editQuoteModal').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget)
   var id_quote = button.data('id_quote') 
   var title = button.data('title')
-  var id_client = button.data('id_client')
-  var client = button.data('client') 
+  // var id_client = button.data('id_client')
+  // var client = button.data('client') 
   var date = button.data('date')
   var date_expired = button.data('date_expired')
 
@@ -13,6 +13,18 @@ $('#editQuoteModal').on('show.bs.modal', function (event) {
   modal.find('.modal-body #title').val(title)
   modal.find('.modal-body #date').val(date)
   modal.find('.modal-body #date_expired').val(date_expired)
-  modal.find('.modal-body #client').prepend("<option value='" + id_client + "' selected>" + client + "</option>")
+  // modal.find('.modal-body #client').prepend("<option value='" + id_client + "' selected>" + client + "</option>")
+
+});
+
+$('#deleteQuoteModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget)
+  var id_quote = button.data('id_quote') 
+  var title = button.data('title')
+
+  var modal = $(this)
+  modal.find('.modal-title').text('Eliminar cotización #' + id_quote)
+  modal.find('.modal-body #id_quote').val(id_quote)
+  modal.find('.modal-body #message').html('¿Estas segur@ de eliminar el registro <strong>"' + title + '"</strong>?')
 
 });
