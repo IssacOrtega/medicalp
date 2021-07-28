e2c96d0686556e0d1e3b9f06e4d91ffbb0792a30
Issac Ortega <gissac.ortega@gmail.com>
2021-07-28 01:15:53 -0500

    Se agrego el archivo user.php para los usuarios

---
+++ dashboard/user.php
@@ -0,0 +1,22 @@
+<?php
+
+// Declarando la funcion de sesion iniciada
+session_start();
+
+// Llamando archvio de configuracion
+require '../data/config.php';
+
+// Llamando al archivo de funciones
+require '../data/functions.php';
+
+
+
+
+/* Condicional para verficar si las variable de sesión de usuario esta seteada 
+nos redireccione a la pagina de inicio de la aplicación */
+if (!isset($_SESSION['user'])) {
+    header('Location: ' . RUTA);
+}
+
+// LLamando al archivo de vista de inicio de sesión
+require 'views/user.view.php';
