f84e01f839e1652ce34440a5e8f6f67dcd6fef18
Issac Ortega <gissac.ortega@gmail.com>
2021-08-03 00:59:20 -0500

    Se agregaron funciones para el crud de cotizaciones y sus validaciones

--- data/functions.php
+++ data/functions.php
@@ -29,3 +29,45 @@ function logIn($conexion, $user, $password)
     $result = $statement->fetch();
     return ($result) ? $result : false;
 }
+
+// Funcion para traer todas las cotizaciones de administrador
+function quotations($conexion)
+{
+    $statement = $conexion->query('SELECT a.id_quote, a.id_quote_client, a.title, a.date, a.validity, b.id_client, b.name_client, c.name 
+    FROM quotation a inner JOIN client b ON a.id_client = b.id_client inner JOIN user c ON a.id_user = c.id_user ORDER BY id_quote DESC');
+    $resultado = $statement->fetchAll();
+    return ($resultado) ? $resultado : false;
+}
+
+// Funcion para traer todas las cotizaciones de administrador
+function quotations_user($conexion, $id_user)
+{
+    $statement = $conexion->query("SELECT a.id_quote, a.id_quote_client, a.title, a.date, a.validity, b.id_client, b.name_client, c.name 
+    FROM quotation a inner JOIN client b ON a.id_client = b.id_client inner JOIN user c ON a.id_user = c.id_user WHERE a.id_user = $id_user ORDER BY id_quote DESC");
+    $resultado = $statement->fetchAll();
+    return ($resultado) ? $resultado : false;
+}
+
+// Funcion para traer todas las cotizaciones de administrador
+function quote_product_exist($conexion, $id_quote)
+{
+    $statement = $conexion->query("SELECT * FROM quote_product WHERE id_quote = $id_quote");
+    $resultado = $statement->fetchAll();
+    return ($resultado) ? $resultado : false;
+}
+
+// Funcion para traer todos los clientes
+function all_clients($conexion)
+{
+    $statement = $conexion->query("SELECT * FROM client");
+    $resultado = $statement->fetchAll();
+    return ($resultado) ? $resultado : false;
+}
+
+// Funcion para traer todas las cotizaciones de administrador
+function id_quote_client_exist($conexion, $id_quote_client)
+{
+    $statement = $conexion->query("SELECT id_quote_client FROM quotation WHERE id_quote_client = $id_quote_client");
+    $resultado = $statement->fetchAll();
+    return ($resultado) ? $resultado : false;
+}
