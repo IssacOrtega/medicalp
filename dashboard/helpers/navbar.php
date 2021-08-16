9d97c50b0c08db7a0c6df982f3bf23fe09bc949c
Issac Ortega <gissac.ortega@gmail.com>
2021-08-16 03:44:54 -0500

    Se agrego el campo de busqueda a la navbar

--- dashboard/helpers/navbar.php
+++ dashboard/helpers/navbar.php
@@ -3,11 +3,15 @@
         <img class="mb-1" src="<?php echo RUTA . 'resource/assets/brand/Logo.png' ?>" alt="Medical Planet" width="20" height="20">
         Medical Planet
     </a>
-    <div class="text-white">&nbsp;Administrador de Cotizaciones</div>
+    <!-- <div class="text-white">&nbsp;Administrador de Cotizaciones</div> -->
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
+    <?php if ($_SERVER['PHP_SELF'] != MENU . 'quote.product.php') : ?>
+        <form class="w-100" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+            <input class="form-control form-control-dark w-100" type="text" name="search" placeholder="Buscar" aria-label="Search">
+        </form>
+    <?php endif; ?>
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
             <a class="nav-link" href="<?php echo RUTA . 'dashboard/routes/logout.php'; ?>">Cerrar Sesión</a>
