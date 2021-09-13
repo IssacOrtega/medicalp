491b910bf7ded99bf5ff9f8b9208bed6493505e4
Issac Ortega <gissac.ortega@gmail.com>
2021-09-13 00:27:58 -0500

    Se cambio el setlocal por lenguaje de mexico

--- data/config.php
+++ data/config.php
@@ -1,7 +1,7 @@
 <?php
 
 date_default_timezone_set('Mexico/General');
-setlocale(LC_ALL, "es_ES");
+setlocale(LC_ALL, "es_MX");
 
 // Borrar reportes de error para produccion
 // error_reporting(0);
