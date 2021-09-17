d73a3cd3bffc1901d43e2af151930843b526ee59
Issac Ortega <gissac.ortega@gmail.com>
2021-09-16 23:11:55 -0500

    Se agregaron las respectivas variables para mostrar los datos de productos y algunas otras funciones

--- dashboard/views/product.view.php
+++ dashboard/views/product.view.php
@@ -16,41 +16,70 @@
                         <button type="button" class="btn btn-sm btn-outline-success" data-toggle="modal" data-target="#productModal">+ Nuevo producto</button>
                     </div>
                 </div>
+                <?php if (!empty($msg)) {
+                    echo $msg;
+                } ?>
                 <div class="table-responsive">
-                    <table class="table table-striped table-sm">
-                        <thead class="thead-dark">
-                            <tr>
-                                <th>Imagen</th>
-                                <th>Producto</th>
-                                <th>Marca</th>
-                                <th>Precio unitario sin IVA</th>
-                                <th>Editar</th>
-                                <th>Eliminar</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td class="align-middle"><img src="<?php echo RUTA . 'resource/assets/products/mesa.jpg' ?>" alt="" width="60" height="60"></td>
-                                <td class="align-middle">Mesa Riñon 1 Cercha Acero Inox</td>
-                                <td class="align-middle">MEDPLAN</td>
-                                <td class="align-middle">$4830.00</td>
-                                <td class="align-middle">
-                                    <center><button class="btn btn-primary" data-toggle="modal" data-target="#productEditModal" data-id_product="1" data-product="Mesa Riñon 1 Cercha Acero Inox" data-brand="MEDPLAN" data-image='<img src="<?php echo RUTA . 'resource/assets/products/mesa.jpg' ?>" class="col-12">'><img src="<?php echo RUTA . 'resource/assets/icons/edit.png' ?>" alt="Editar" width="20" height="20"></button></center>
-                                </td>
-                                <td class="align-middle">
-                                    <center><button class="btn btn-danger" data-toggle="modal" data-target="#productDeleteModal" data-id_product="1" data-product="Mesa Riñon 1 Cercha Acero Inox"><img src="<?php echo RUTA . 'resource/assets/icons/trash.png' ?>" alt="Eliminar" width="20" height="20"></button></center>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td colspan="7">
-                                    <strong style="font-size: medium;">Descripción:</strong><br>
-                                    El concentrador de oxígeno Stratus 5 producido por 3B ™ Medical Solutions puede suministrar a un paciente con oxígeno constante en un flujo ajustable, seguro, confiable, de bajo costo.
-                                    Este concentrador está cuidadosamente diseñado teniendo en cuenta el rendimiento y la fiabilidad, incluidas características únicas, como un innovador sistema de refrigeración para proteger los lechos de tamices y un monitor de pureza de oxígeno.
-                                    El bajo mantenimiento, el funcionamiento ultra silencioso y la mayor presión de salida para garantizar una entrega adecuada hacen que esta unidad sea ideal para su uso en hogares, instituciones, vehículos y otros entornos móviles diversos
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <?php if ($products) : ?>
+                        <table class="table table-striped table-sm">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th>Imagen</th>
+                                    <th>Producto</th>
+                                    <th>Marca</th>
+                                    <th>Precio unitario sin IVA</th>
+                                    <th>Editar</th>
+                                    <th>Eliminar</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($products as $product) : ?>
+                                    <tr>
+                                        <td class="align-middle"><img src="<?php echo RUTA . $product['image']; ?>" alt="" width="60" height="60"></td>
+                                        <td class="align-middle"><?php echo $product['product']; ?></td>
+                                        <td class="align-middle"><?php echo $product['brand']; ?></td>
+                                        <td class="align-middle">$ <?php echo number_format($product['unit_price'], 2); ?> MXN</td>
+                                        <td class="align-middle">
+                                            <center><button class="btn btn-primary" data-toggle="modal" data-target="#productEditModal" data-id_product="<?php echo $product['id_product']; ?>" data-product="<?php echo $product['product']; ?>" data-brand="<?php echo $product['brand']; ?>" data-iva="<?php echo $product['iva']; ?>" data-unit_price="<?php echo $product['unit_price']; ?>" data-description="<?php echo $product['description']; ?>" data-img_name="<?php echo $product['image']; ?>" data-image='<img src="<?php echo RUTA . $product['image']; ?>" class="col-12">'><img src="<?php echo RUTA . 'resource/assets/icons/edit.png' ?>" alt="Editar" width="20" height="20"></button></center>
+                                        </td>
+                                        <td class="align-middle">
+                                            <center><button class="btn btn-danger" data-toggle="modal" data-target="#productDeleteModal" data-id_product="<?php echo $product['id_product']; ?>" data-product="<?php echo $product['product']; ?>" data-image="<?php echo $product['image']; ?>"><img src="<?php echo RUTA . 'resource/assets/icons/trash.png' ?>" alt="Eliminar" width="20" height="20"></button></center>
+                                        </td>
+                                    </tr>
+                                    <?php if ($product['description'] != null) : ?>
+                                        <tr>
+                                            <td colspan="7">
+                                                <strong style="font-size: medium;">Descripción:</strong><br>
+                                                <?php echo nl2br($product['description']); ?>
+                                            </td>
+                                        </tr>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    <?php else : ?>
+                        <table class="table table-striped table-sm">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th>Imagen</th>
+                                    <th>Producto</th>
+                                    <th>Marca</th>
+                                    <th>Precio unitario sin IVA</th>
+                                    <th>Editar</th>
+                                    <th>Eliminar</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th colspan="7">
+                                        <center>
+                                            <strong style="font-size: 25px;">No hay datos.</strong>
+                                        </center>
+                                    </th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    <?php endif; ?>
                 </div>
             </main>
         </div>
@@ -67,7 +96,7 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <form action="#" method="POST">
+                    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" enctype="multipart/form-data">
                         <div class="form-group">
                             <label for="product" class="col-form-label">Producto:</label>
                             <input type="text" class="form-control" id="product" name="product" required>
@@ -79,7 +108,7 @@
                         <div class="form-group">
                             <label for="iva" class="col-form-label">Porcentaje de impuesto (IVA):</label>
                             <div class="input-group">
-                                <input type="number" class="form-control" id="iva" name="iva" placeholder="%" value="16" required>
+                                <input type="number" class="form-control" id="iva" name="iva" placeholder="%" value="0.16" required>
                                 <div class="input-group-append">
                                     <span class="input-group-text">%</span>
                                 </div>
@@ -91,7 +120,7 @@
                         </div>
                         <div class="form-group">
                             <label for="description" class="col-form-label">Descripción:</label>
-                            <textarea class="form-control" id="description" placeholder="Opcional"></textarea>
+                            <textarea class="form-control" id="description" name="description" placeholder="Opcional"></textarea>
                         </div>
                         <label for="exampleFormControlFile1">Foto:</label>
                         <div class="custom-file">
@@ -119,7 +148,7 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <form action="#" method="POST" enctype="multipart/form-data">
+                    <form action="<?php echo htmlspecialchars(RUTA . '/dashboard/routes/product/edit.php'); ?>" method="POST" enctype="multipart/form-data">
                         <input type="hidden" id="id_product" name="id_product">
                         <div class="form-group">
                             <label for="product" class="col-form-label">Producto:</label>
@@ -132,7 +161,7 @@
                         <div class="form-group">
                             <label for="iva" class="col-form-label">Porcentaje de impuesto (IVA):</label>
                             <div class="input-group">
-                                <input type="number" class="form-control" id="iva" name="iva" placeholder="%" value="16" required>
+                                <input type="number" class="form-control" id="iva" name="iva" placeholder="%" value="0.16" required>
                                 <div class="input-group-append">
                                     <span class="input-group-text">%</span>
                                 </div>
@@ -144,11 +173,12 @@
                         </div>
                         <div class="form-group">
                             <label for="description" class="col-form-label">Descripción:</label>
-                            <textarea class="form-control" id="description" placeholder="Opcional"></textarea>
+                            <textarea class="form-control" id="description" name="description" placeholder="Opcional"></textarea>
                         </div>
                         <label for="exampleFormControlFile1">Foto:</label>
                         <div class="custom-file">
-                            <input type="file" class="custom-file-input" id="image" name="image" required>
+                            <input type="hidden" id="image_save" name="image_save">
+                            <input type="file" class="custom-file-input" id="image" name="image">
                             <label class="custom-file-label" for="image">Imagen de producto</label>
                         </div>
                         <hr>
@@ -156,7 +186,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                    <button type="submit" class="btn btn-primary">Crear</button>
+                    <button type="submit" class="btn btn-primary">Actualizar</button>
                 </div>
                 </form>
             </div>
@@ -173,10 +203,11 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <form action="#" method="POST">
+                <form action="<?php echo htmlspecialchars(RUTA . '/dashboard/routes/product/delete.php'); ?>" method="POST">
                     <div class="modal-body">
                         <p id="message">¿Estas segur@ de eliminar el registro?</p>
                         <input type="hidden" id="id_product" name="id_product">
+                        <input type="hidden" id="image" name="image">
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
