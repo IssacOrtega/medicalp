fc8287a4622262b0c032fb40c70308f68bfbeec1
Issac Ortega <gissac.ortega@gmail.com>
2021-09-16 23:15:19 -0500

    Se agregaron los datos para conectar la aplicacion con el servidor de produccion

--- data/config.php
+++ data/config.php
@@ -4,21 +4,21 @@ date_default_timezone_set('Mexico/General');
 setlocale(LC_ALL, "es_MX");
 
 // Borrar reportes de error para produccion
-// error_reporting(0);
+error_reporting(0);
 
 // Variable definida para utilizar en los enlaces o en la reedirección
-define('RUTA', "http://{$_SERVER['SERVER_NAME']}/cotizaciones/");
+define('RUTA', "https://{$_SERVER['SERVER_NAME']}/");
 
-define('MENU', "/cotizaciones/dashboard/");
+define('MENU', "/dashboard/");
 
 //  Arreglo de configuracion de la Base de datos
 $bd_config = array(
     // Nombre de dominio o host
-    'host' => '127.0.0.1:3306',
+    'host' => 'localhost:3306',
     // nombre de la base de datos
     'db' => '_quotes_medicalp',
     // Usurio de base de datos
-    'user' => 'root',
+    'user' => 'iortega',
     // Contraseña de base de datos
-    'pass' => ''
+    'pass' => 'Issac1997*'
 );
