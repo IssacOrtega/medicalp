dfb4de3f19a5aea97e37689cbd433ed6d76e9b7a
Issac Ortega <gissac.ortega@gmail.com>
2021-09-20 03:14:21 -0500

    Se agregaron las variables para mostrar los datos de la bd en la vista de clientes y se modificaron algunos atributos de los inputs

--- dashboard/views/client.view.php
+++ dashboard/views/client.view.php
@@ -16,35 +16,65 @@
                         <button type="button" class="btn btn-sm btn-outline-success" data-toggle="modal" data-target="#newClientModal">+ Nuevo cliente</button>
                     </div>
                 </div>
+                <?php if (!empty($msg)) {
+                    echo $msg;
+                } ?>
                 <div class="table-responsive">
-                    <table class="table table-striped table-sm">
-                        <thead class="thead-dark">
-                            <tr>
-                                <th>Nombre</th>
-                                <th>Lada/Teléfono</th>
-                                <th>Email</th>
-                                <th>Institución</th>
-                                <th>Dirección</th>
-                                <th>Editar</th>
-                                <th>Eliminar</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td class="align-middle">Karina Emiliano Arellano</td>
-                                <td class="align-middle"><a href="tel:5610803992">5610803992</a></td>
-                                <td class="align-middle"><a href="mailto:carinaemiliano12@hotmail.com">carinaemiliano12@hotmail.com</a></td>
-                                <td class="align-middle">Technology Intelligence IK</td>
-                                <td class="align-middle"><button class="btn btn-link" data-toggle="modal" data-target="#addressModal" data-name="Karina Emiliano Arellano" data-state="Edo. de México" data-municipality="Atizapán de Zaragoza 52929" data-address="Boulevard S.O.P #313 #12 Lt.1 Mz.8 52929, Atizapán de Zaragoza, Ciudad Lópex Mateos, Edo de Méx.">Ver</button></td>
-                                <td class="align-middle">
-                                    <center><button class="btn btn-primary" data-toggle="modal" data-target="#editClientModal" data-id_client="1" data-name="Karina Emiliano Arellano" data-phone="5610803992" data-business="Technology Intelligence IK" data-email="carinaemiliano12@hotmail.com" data-state="Edo. de México" data-municipality="Atizapán de Zaragoza 52929" data-address="Boulevard S.O.P #313 #12 Lt.1 Mz.8 52929, Atizapán de Zaragoza, Ciudad Lópex Mateos, Edo de Méx."><img src="<?php echo RUTA . 'resource/assets/icons/edit.png' ?>" alt="Imprimir" width="20" height="20"></button></center>
-                                </td>
-                                <td class="align-middle">
-                                    <center><button class="btn btn-danger" data-toggle="modal" data-target="#deleteClientModal" data-id_client="1" data-name="Karina Emiliano Arellano"><img src="<?php echo RUTA . 'resource/assets/icons/trash.png' ?>" alt="Imprimir" width="20" height="20"></button></center>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <?php if ($clients) : ?>
+                        <table class="table table-striped table-sm">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th>Nombre</th>
+                                    <th>Lada/Teléfono</th>
+                                    <th>Email</th>
+                                    <th>Institución</th>
+                                    <th>Dirección</th>
+                                    <th>Editar</th>
+                                    <th>Eliminar</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($clients as $client) : ?>
+                                    <tr>
+                                        <td class="align-middle"><?php echo $client['name_client']; ?></td>
+                                        <td class="align-middle"><a href="tel:<?php echo $client['phone_client']; ?>"><?php echo $client['phone_client']; ?></a></td>
+                                        <td class="align-middle"><?php echo ($client['email_client'] == null) ? 'Sin registro' : '<a href="mailto:' . $client['email_client'] . '">' . $client['email_client'] . '</a>'; ?></td>
+                                        <td class="align-middle"><?php echo $client['business']; ?></td>
+                                        <td class="align-middle"><button class="btn btn-link" data-toggle="modal" data-target="#addressModal" data-name="<?php echo $client['name_client']; ?>" data-state="<?php echo $client['state']; ?>" data-municipality="<?php echo ($client['municipality'] == null) ? 'Sin registro.' : $client['municipality']; ?>" data-address="<?php echo ($client['address'] == null) ? 'Sin registro.' : $client['address']; ?>">Ver</button></td>
+                                        <td class="align-middle">
+                                            <center><button class="btn btn-primary" data-toggle="modal" data-target="#editClientModal" data-id_client="<?php echo $client['id_client']; ?>" data-name="<?php echo $client['name_client']; ?>" data-phone="<?php echo $client['phone_client']; ?>" data-business="<?php echo $client['business']; ?>" data-email="<?php echo ($client['email_client'] == null) ? '' : $client['email_client']; ?>" data-state="<?php echo $client['state']; ?>" data-municipality="<?php echo ($client['municipality'] == null) ? '' : $client['municipality']; ?>" data-address="<?php echo ($client['address'] == null) ? '' : $client['address']; ?>"><img src="<?php echo RUTA . 'resource/assets/icons/edit.png' ?>" alt="Imprimir" width="20" height="20"></button></center>
+                                        </td>
+                                        <td class="align-middle">
+                                            <center><button class="btn btn-danger" data-toggle="modal" data-target="#deleteClientModal" data-id_client="<?php echo $client['id_client']; ?>" data-name="<?php echo $client['name_client']; ?>"><img src="<?php echo RUTA . 'resource/assets/icons/trash.png' ?>" alt="Imprimir" width="20" height="20"></button></center>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    <?php else : ?>
+                        <table class="table table-striped table-sm">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th>Nombre</th>
+                                    <th>Lada/Teléfono</th>
+                                    <th>Email</th>
+                                    <th>Institución</th>
+                                    <th>Dirección</th>
+                                    <th>Editar</th>
+                                    <th>Eliminar</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th colspan="7">
+                                        <center>
+                                            <strong style="font-size: 25px;">No hay datos.</strong>
+                                        </center>
+                                    </th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    <?php endif; ?>
                 </div>
             </main>
         </div>
@@ -61,7 +91,7 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <form action="#" method="POST">
+                    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
                         <div class="form-group">
                             <label for="name" class="col-form-label">Nombre completo:</label>
                             <input type="text" class="form-control" id="name" name="name" required>
@@ -136,7 +166,7 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <form action="#" method="POST">
+                    <form action="<?php echo htmlspecialchars(RUTA . '/dashboard/routes/client/edit.php'); ?>" method="POST">
                         <input type="hidden" id="id_client" name="id_client">
                         <div class="form-group">
                             <label for="name" class="col-form-label">Nombre completo:</label>
@@ -152,11 +182,11 @@
                         </div>
                         <div class="form-group">
                             <label for="email" class="col-form-label">Email:</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
+                            <input type="email" class="form-control" id="email" name="email" placeholder="(Opcional)">
                         </div>
                         <div class="form-group">
                             <label for="state" class="col-form-label">País/Estado/Zona:</label>
-                            <input type="text" class="form-control" id="state" name="state" placeholder="(Opcional)">
+                            <input type="text" class="form-control" id="state" name="state" required>
                         </div>
                         <div class="form-group">
                             <label for="municipality" class="col-form-label">Municipio/C.P.:</label>
@@ -186,7 +216,7 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <form action="#" method="POST">
+                <form action="<?php echo htmlspecialchars(RUTA . '/dashboard/routes/client/delete.php'); ?>" method="POST">
                     <div class="modal-body">
                         <p id="message">¿Estas segur@ de eliminar el registro de?</p>
                         <input type="hidden" id="id_client" name="id_client">
