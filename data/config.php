e10b07bb4f495f32864ed41a4671f68e7841da2a
Issac Ortega <gissac.ortega@gmail.com>
2021-09-20 03:21:44 -0500

    Se agregaron los datos del servidor de produccion

--- data/config.php
+++ data/config.php
@@ -2,11 +2,12 @@
 
 date_default_timezone_set('Mexico/General');
 setlocale(LC_ALL, "es_MX");
+header('Content-Type: text/html; charset=utf-8');
 
 // Borrar reportes de error para produccion
 error_reporting(0);
 
-// Variable definida para utilizar en los enlaces o en la reedirección
+// Variable definida para utilizar en los enlaces o en la reedirecci贸n
 define('RUTA', "https://{$_SERVER['SERVER_NAME']}/");
 
 define('MENU', "/dashboard/");
@@ -14,11 +15,11 @@ define('MENU', "/dashboard/");
 //  Arreglo de configuracion de la Base de datos
 $bd_config = array(
     // Nombre de dominio o host
-    'host' => 'localhost:3306',
+    'host' => '192.169.151.159',
     // nombre de la base de datos
     'db' => '_quotes_medicalp',
     // Usurio de base de datos
     'user' => 'iortega',
-    // Contraseña de base de datos
+    // Contrase帽a de base de datos
     'pass' => 'Issac1997*'
 );
